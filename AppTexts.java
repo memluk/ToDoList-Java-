c9440d873e8689e57f3e0a17a3d0a6024395ad3f
Murat Yaşar <89690903+memluk@users.noreply.github.com>
2022-10-20 12:43:58 +0200

    Create AppTexts.java

---
+++ AppTexts.java
@@ -0,0 +1,28 @@
+/**
+ * This class contains all the texts and messages of the program
+ * TODO 4: Create AppTexts class outsource the text-contents
+ */
+public class AppTexts {
+    //region Constants
+    public static final String MAIN_MENU_SHOW = "\n[\t" + AppCommands.USER_CMD_SHOW + "\t] Show all notes";
+    public static final String MAIN_MENU_CREATE = "[\t" + AppCommands.USER_CMD_CREATE + "\t] Create a note";
+    public static final String MAIN_MENU_EDIT = "[\t" + AppCommands.USER_CMD_EDIT + "\t] Edit a note";
+    public static final String MAIN_MENU_DELETE = "[\t" + AppCommands.USER_CMD_DELETE + "\t] Delete a note";
+    public static final String MAIN_MENU_EXIT = "[\t" + AppCommands.USER_CMD_EXIT + "\t] End the program";
+    public static final String MSG_USER_CHOICE = "\nChoose your option:";
+    public static final String MSG_INVALID_CHOICE = "\nUndefined Choice!";
+    //endregion
+
+    //region Attributes
+    //endregion
+
+    /**
+     * Private constructor to prevent creation of a new object from outside
+     */
+    //region Constructor
+    private AppTexts() {}
+    //endregion
+
+    //region Methods
+    //endregion
+}
