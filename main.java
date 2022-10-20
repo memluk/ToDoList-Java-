4b9025af1c1ad5440c8ed308bb1a44264c929109
Murat Yaşar <89690903+memluk@users.noreply.github.com>
2022-10-20 12:38:32 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * Start Method of Main Program
+     * @param args : {@link String[]} : Startparameter
+     */
+    public static void main(String[] args) {
+        UIController uiControl = new UIController();
+        uiControl.startUI();
+    }
+}
