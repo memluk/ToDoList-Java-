23b31626bad4174518f03d2716d1dabed67241fb
Murat Yaşar <89690903+memluk@users.noreply.github.com>
2022-10-20 15:35:32 +0200

    Create NoteSettings.java
    Settings for notes has been added

---
+++ NoteSettings.java
@@ -0,0 +1,21 @@
+/**
+ * This class contains the general settings of user-input
+ * TODO 2 : Implement NoteSettings class and initialize the values
+ */
+public class NoteSettings {
+    //region Constants
+    public static final int MIN_LENGTH_TITLE = 3;
+    public static final int MAX_LENGTH_TITLE = 20;
+    public static final int MAX_LENGTH_CONTENT = 100;
+    //endregion
+
+    //region Attributes
+    //endregion
+
+    //region Constructor
+    private NoteSettings() {}
+    //endregion
+
+    //region Methods
+    //endregion
+}
