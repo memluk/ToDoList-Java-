864e30cf9c939116ba59285f67068cef5ca1652b
Murat Yaşar <89690903+memluk@users.noreply.github.com>
2022-10-24 16:05:31 +0200

    Update Note.java
    New methods have been added:
         - getAttributesAsCsvLine()
         - setAttributesAsCsvLine()

--- Note.java
+++ Note.java
@@ -1,12 +1,13 @@
 /**
- * Note class is the Model class which has a titel, content and a flag,
- * which shows whether the note is important.
- * TODO 1 : Create and implement Model class
+ * Note class is the Model class which has a titel, content and a flag, which shows whether the note is important.
  */
 public class Note {
     //region Constants
-    private static final String DEF_STR_VAL = ">noData<";
+    public static final String DEF_STR_VAL = ">noData<";
     private static final boolean DEF_BOOL_VAL = false;
+    public static final int INDEX_TITLE = 0;
+    public static final int INDEX_CONTENT = 1;
+    public static final int INDEX_IS_IMPORTANT = 2;
     //endregion
 
     //region Attributes
@@ -36,10 +37,38 @@ public class Note {
         this.content = content;
         this.isImportant = isImportant;
     }
+    
+    /**
+    * Overloaded constructor accepting a CSV line and initializing all attributes using a method.
+    * @param csvLine : {@link String} CVS-line
+    */
+    public Note(String csvLine) {
+        setAttributesFromCsvLine(csvLine);
+    }
+    
     //endregion
 
     //region Methods
+    /**
+     * Distribute Attributes as CVS
+     * @return {@link String} : CVS-line
+     */
+    public String getAttributesAsCsvLine() {
+        return title + FileHandler.DELIMITER + content + FileHandler.DELIMITER + isImportant + "\n";
+    }
+    
+     /**
+     * Fill out the attributes with CVS-Strings
+     * @return csvLine : {@link String} : Csv-String with Attributes
+     */
+    public void setAttributesFromCsvLine(String csvLine) {
+        String[] allAttributes = csvLine.split(FileHandler.DELIMITER);
 
+        title = allAttributes[INDEX_TITLE];
+        content = allAttributes[INDEX_CONTENT];
+        isImportant = Boolean.parseBoolean(allAttributes[INDEX_IS_IMPORTANT]);
+    }
+    
     public String getTitle() {return title;}
 
     public void setTitle(String title) {this.title = title;}
