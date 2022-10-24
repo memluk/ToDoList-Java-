6499cda76aeae1ae4a30ed35793588bc8670581b
Murat Yaşar <89690903+memluk@users.noreply.github.com>
2022-10-24 15:55:29 +0200

    Update AppTexts.java
    New messages have been added.

--- AppTexts.java
+++ AppTexts.java
@@ -1,6 +1,5 @@
 /**
  * This class contains all the texts and messages of the program
- * TODO 4 : Create AppTexts class outsource the text-contents
  */
 public class AppTexts {
     //region Constants
@@ -14,12 +13,26 @@ public class AppTexts {
     public static final String MSG_INVALID_CHOICE = "\nUndefined Choice!";
     public static final String MSG_CHOOSE_INDEX_DELETE = "\nSelect a note to delete: ";
     public static final String MSG_DELETE_SUCCESSFUL = "\nThe note has been successfully deleted.";
-    
     public static final String MSG_INPUT_NOTE_DATA = "\nPlease, enter the following information: ";
+    
     public static final String MSG_INPUT_TITLE = "Titel: ";
     public static final String MSG_INPUT_CONTENT = "Content: ";
-    public static final String MSG_INPUT_IMPORTANT = "Is this note important?";
+    public static final String MSG_INPUT_IMPORTANT = "Is this note important? ( Y / N ) : ";
     public static final String MSG_CHOOSE_INDEX_EDIT = "\nPlease, select the note, you want to edit: ";
+    
+    public static final String MSG_INVALID_TITLE = "\nInvalid Titel! The Titel must have %d to %d characters.\n";
+    public static final String MSG_INVALID_CONTENT = "\nInvalid Content! The content can have max %d characters.\n";
+    public static final String MSG_INVALID_IMPORTANCE = "\nInvalid input! Please enter 'Y' for yes or 'N' for no!\n";
+    public static final String MSG_NOTE_CREATION_SUCCESS = "\nYour note has been successfully created.";
+    public static final String MSG_NOTE_EDIT_SUCCESS = "\nYour note has been successfully updated.";
+    
+    public static final String FORMAT_STRING_NOTE_HEADER = "\n%5s %20s %40s %10s\n";
+    public static final String FORMAT_STRING_NOTE = "%5d %20s %40s %10s\n";
+    
+    public static final String INDEX = "Index";
+    public static final String CONTENT = "Content";
+    public static final String IMPORTANT = "Important";
+    public static final String TITLE = "Titel";
     //endregion
 
     //region Attributes
